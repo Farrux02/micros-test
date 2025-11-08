d657d1153885850c2aa234b30845d47fe34f1902
Farrux Adxamov <farruh020202@gmail.com>
2025-11-08 16:18:12 +0500

    chore: add vuetify config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    primary: '#1976d2',
+                    secondary: '#424242',
+                    accent: '#82b1ff',
+                    error: '#ff5252',
+                    info: '#2196f3',
+                    success: '#4caf50',
+                    warning: '#fb8c00'
+                }
+            }
+        }
+    }
+})
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.mount('#app')
